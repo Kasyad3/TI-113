29ecc2460ad675fe1d6633b79e941848b34143f2
Kasyad3 <paulo.hprosa@senacsp.edu.br>
2024-09-11 19:53:16 -0300

    feat: Exercício 1 concluído.

---
+++ ex1/script.js
@@ -0,0 +1,42 @@
+
+let numeros = listaNumeros()
+function listaNumeros() {
+    let numeros = [];
+    
+    for (i = 0; i < 5; i++) {
+        let numero
+        
+        
+        do {
+            numero = parseFloat(prompt(`Informe o ${i + 1}° número: `));
+            
+            if (isNaN(numero))
+                alert(`Valor inválido, informe um número.`)
+            
+        } while (isNaN(numero))
+            
+            numeros.push(numero)
+            
+        }
+        return numeros;
+    }
+
+exibirNumeros(numeros)
+function exibirNumeros(numeros) {
+let mensagem = ""
+    for (i = 0; i < numeros.length; i++) {
+
+        if (i == numeros.length - 1)
+            mensagem += numeros[i] + "."
+
+        else if (i == numeros.length - 2)
+            mensagem += numeros[i] + " e "
+
+        else
+        mensagem+= numeros[i] + ", "
+
+       
+
+    }
+    alert(mensagem)
+}
